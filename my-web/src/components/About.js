fd0eddc1ee28650aa121a7b79a767fb7844bd67b
Safi Shahid <96895832+SafiShahid34@users.noreply.github.com>
2023-12-20 22:27:39 -0500

    fixed scaling and added projects

--- my-web/src/components/About.js
+++ my-web/src/components/About.js
@@ -3,46 +3,49 @@ import AboutImg from "../assets/about-img.png";
 
 const About = () => {
   return (
-    <section className="bg-secondery text-white px-5 py-16" id="about">
+    <section className="bg-secondery text-white px-5 py-12" id="about">
       <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
         <div className="about-info ml-12 ">
-          <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
+          <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[174px] border-indigo-600 pb-2">
             About Me
           </h2>
 
-          <p className="pb-5">
+          <p className="pb-5 " >
             Hi, My Name Is Safi Shahid I am a Full Stack Developer in New York. 
             During my pursuits in computer science, i have developed a strong admiration for web technologies
             Most notably in React and Node.js.
           </p>
+          <div className="ml-8 mt-4">
           <p className="pb-5">
-            <p className="font-bold">Languages/Tools:</p> <br></br>
+            <p className="font-bold border-b-[5px] w-[146px] border-indigo-600 ">Languages/Tools:</p> <br></br>
           	React, Angular, Node.js, SQL, PHP,  Javascript, Typescript, Python, Java, Scala, C/ C++, JQuery,
-Django, MongoDB, R, .Net, Go, Kotlin, GraphQL, Vue.js, Spring Boot
+            Django, MongoDB, R, .Net, Go, Kotlin, GraphQL, Vue.js, Spring Boot
 
-<p className="font-bold"></p> <br></br>
+            <p className="font-bold"></p> <br></br>
             Docker, BootStrap, Git, MySQL, Visio, JIRA, Oracle, Excel, AWS, Firebase, Tabuleu
 
           </p>
 
 
           <p className="pb-5">
-            <p className="font-bold">Experience:</p><br></br>
+            <p className="font-bold border-b-[5px] w-[98px] border-indigo-600">Experience:</p><br></br>
             <p>The Ticketing Co. </p>
             <p>Software Engineer Intern</p>
             <p>2023</p>
           </p>
 
           <p >
-            <p className="font-bold">Education:</p><br></br>
-            <p>University at Buffalo, The State University of New York, Computer Science		
+            <p className="font-bold border-b-[5px] w-[98px] border-indigo-600">Education:</p><br></br>
+            <p>University at Buffalo, The State University of New York, Computer Science</p>		
             <p>Buffalo, NY, May 2024</p>
           </p>
+
+          </div>
 
 
 
             
-          </p>
+          
         </div>
 
         <div className="about-img">
--- my-web/src/components/Projects.js
+++ my-web/src/components/Projects.js
@@ -1,9 +1,10 @@
 import React from "react";
-import cssProjects from "../assets/cssprojects.png";
-import devlog from "../assets/devlog.png";
-import getInspirred from "../assets/get-inspirred.png";
-import uilogs from "../assets/uilogs.png";
 import EduTwo from "../assets/EduTwo.png";
+import PVss from "../assets/PVss.png";
+import VSim from "../assets/VSim.png"
+import KWA from "../assets/KWA.png";
+import OWC from "../assets/OWC.png";
+import CC from "../assets/CC.png"
 
 
 const Projects = () => {
@@ -15,12 +16,47 @@ const Projects = () => {
       live: "https://edublockchain.netlify.app/",
       code: "https://github.com/stepankriminskiy/EduBlockchain",
     },
+    {
+      img: PVss,
+      title: "PVss",
+      desc: "  Dijikstras Visual Pathfinder ",
+      live: "https://edublockchain.netlify.app/",
+      code: "https://github.com/SafiShahid34/VisualPathfinder",
+    },
+    {
+      img: VSim,
+      title: "VSim",
+      desc: "  Virus Simulation Staticstics with graphical representation ",
+      live: "https://edublockchain.netlify.app/",
+      code: "https://github.com/SafiShahid34/VirusSim",
+    },
+    {
+      img: KWA,
+      title: "KWA",
+      desc: "  Weather react app using OpenWeather API ",
+      live: "https://edublockchain.netlify.app/",
+      code: "https://github.com/SafiShahid34/React-Kelvin-Weather",
+    },
+    {
+      img: OWC,
+      title: "OWC",
+      desc: "  Online Chat Application using MySQL ",
+      live: "https://edublockchain.netlify.app/",
+      code: "https://github.com/SafiShahid34/OnlineWebChat",
+    },
+    {
+      img: CC,
+      title: "CC",
+      desc: "  Calorie Counter in JS ",
+      live: "https://edublockchain.netlify.app/",
+      code: "https://github.com/SafiShahid34/CalCounter",
+    },
     
   ];
 
   return (
     <section className="bg-primary text-white px-5 py-16" id="projects">
-      <div className="container ml-12 grid md:grid-cols-2 items-center md:justify-between">
+      <div className="container grid md:grid-cols-2 items-center md:justify-between">
         <div className="about-info mb-5">
           <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
             Projects
@@ -34,11 +70,11 @@ const Projects = () => {
         <div className="about-img"></div>
       </div>
 
-      <div className="projects container ml-12 grid md:grid-cols-3 gap-10">
+      <div className="projects container grid md:grid-cols-3 gap-10">
         {projects.map((project, i) => {
           return (
-            <div className="relative" key={i}>
-              <img src={project.img} alt={project.title} />
+            <div className="relative " key={i}>
+              <img src={project.img} alt={project.title} className="w-full h-52 object-cover rounded-lg"/>
               <div className="flex absolute left-0 right-0 top-[13px] bottom-0 mx-auto w-[90%] h-[90%]  bg-primary  opacity-0 duration-500 justify-center flex-col hover:opacity-100 ">
                 <p className="py-5 text-center font-bold px-2 text-white">
                   {project.desc}
